c3d4f1d8f146b6b57b02827a9cb5312f4ba965da
Maëlys Lerat <mlerat@student.ethz.ch>
2020-03-05 16:50:44 +0100

    Added the p norm function in point class

--- code/src/main/scala/ch/ethz/math/ifor/rectanglePacking/Point.scala
+++ code/src/main/scala/ch/ethz/math/ifor/rectanglePacking/Point.scala
@@ -5,5 +5,14 @@ abstract class Point(val coordinates: Vector[Double]) {
 
   def dominates(point: Point): Boolean = (coordinates zip point.coordinates).forall(pair => pair._1 < pair._2)
 
+  def norm(p: Double)=  {
+    assert(p>=1)
+    if (p==inf) {
+      coordinates.map(_ .abs).max
+    }
+    else {
+      Math.pow(coordinates.map(a => Math.pow(a.abs,p)).sum,1/p)
+    }
+  }
 
 }
