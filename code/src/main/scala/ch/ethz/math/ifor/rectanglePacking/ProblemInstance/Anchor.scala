91218c31c4c7336c5f50590ee28c65e4ae0f0aa3
Federica95 <fcecchetto@ethz.ch>
2020-03-12 19:43:43 +0100

    added documentation

--- code/src/main/scala/ch/ethz/math/ifor/rectanglePacking/ProblemInstance/Anchor.scala
+++ code/src/main/scala/ch/ethz/math/ifor/rectanglePacking/ProblemInstance/Anchor.scala
@@ -12,14 +12,15 @@ class Anchor(coordinates:Vector[Double]) extends Point(coordinates) {
 }
 object Anchor{
   /**
-    * create a list of random Anchors (draw from [0,1]x[0,1] uniformly at random)
+    * create a random Anchor in the right dimension (draw from [0,1]x[0,1] uniformly at random)
     */
   def randomAnchor: Anchor = new Anchor(
     (for (i<- 1 to dimension) yield math.random).toVector
   )
 
-  /** //TODO: add documentation
-     */
+  /**
+    * create a list of random Anchors (draw from [0,1]x[0,1] uniformly at random)
+    */
   def random(n: Int): List[Anchor] =
     (for (i<- 1 to n) yield Anchor.randomAnchor).toList
 }
