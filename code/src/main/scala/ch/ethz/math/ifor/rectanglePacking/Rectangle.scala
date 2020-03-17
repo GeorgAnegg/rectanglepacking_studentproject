64c0bb25c4ccb4932aa44d19c5904fb5ecd7a90d
Maëlys Lerat <mlerat@student.ethz.ch>
2020-03-17 18:02:36 +0100

    Changed how we define rectangles and added a function to compute boundaries (in progress)

--- code/src/main/scala/ch/ethz/math/ifor/rectanglePacking/Rectangle.scala
+++ code/src/main/scala/ch/ethz/math/ifor/rectanglePacking/Rectangle.scala
@@ -1,5 +1,26 @@
 package ch.ethz.math.ifor.rectanglePacking
 
-class Rectangle {
+import ch.ethz.math.ifor.rectanglePacking.ProblemInstance.Anchor
 
+class Rectangle(val anchorOrigin : Anchor, val anchorTopRight: Anchor ) {
+
+  /**
+    * For a rectangle in dimension "dimension", returns the low boundary corresponding to the hyperplane on dimension i
+    * @param i
+    * @return
+    */
+  def lowBoundary(i : Int): Vector[Double] = {
+    require (i>=0 && i< dimension)
+    var emptyVector: Vector[Double] = Vector.empty[Double]
+    for (j <- 0 until dimension) {
+      if (j==i) {
+          emptyVector= emptyVector :+ anchorOrigin.coordinates(i)
+      }
+      else {
+        emptyVector = emptyVector :+ anchorOrigin.coordinates(j)
+        emptyVector = emptyVector :+ anchorTopRight.coordinates(j)
+      }
+    }
+    emptyVector
+  }
 }
