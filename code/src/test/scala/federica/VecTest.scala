3ad7adc43d34416d1146d021538d0c107015d0c4
Federica95 <fcecchetto@ethz.ch>
2020-03-27 13:46:13 +0100

    Draw rectangles as lists of vectors

---
+++ code/src/test/scala/federica/VecTest.scala
@@ -0,0 +1,24 @@
+package federica
+
+import co.theasi.plotly.{Plot,draw}
+
+object VecTest extends App {
+  println{"Hello Federica"}
+  val xs: Vector[Double]= Vector(0,0,1,1,0)
+  val ys: Vector[Double]= Vector(0,1,1,0,0)
+
+  val x1: Vector[Double]= Vector(0.5,0.5,1)
+  val y1: Vector[Double]= Vector(1,0.5,0.5)
+
+  val x: Vector[Vector[Double]] = Vector(xs,x1)
+  val y: Vector[Vector[Double]] = Vector(ys,y1)
+
+  var plot = Plot()
+  for (i<- 0 until 2){
+    plot=plot.withScatter(x(i),y(i))
+  }
+
+  //val plot = Plot().withScatter(xs, ys)
+
+  draw(plot, "my-first-plot")
+}
