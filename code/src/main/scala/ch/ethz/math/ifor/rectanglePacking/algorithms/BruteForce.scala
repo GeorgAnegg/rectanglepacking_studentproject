43e2beca1013b0d7ddc9a45c36acc42f655e2fac
Maëlys Lerat <mlerat@student.ethz.ch>
2020-04-16 12:54:39 +0200

    New names for bruteforce functions

--- code/src/main/scala/ch/ethz/math/ifor/rectanglePacking/algorithms/BruteForce.scala
+++ code/src/main/scala/ch/ethz/math/ifor/rectanglePacking/algorithms/BruteForce.scala
@@ -12,9 +12,9 @@ object BruteForce extends Algorithm {
     * @param allAnchors
     * @return
     */
-  def runAux(instance: Instance,possibleTops:List[Point],allAnchors:List[Anchor]):Map[Anchor,Rectangle] = {
+  def runDepth(instance: Instance, possibleTops:List[Point], allAnchors:List[Anchor]):Map[Anchor,Rectangle] = {
     if (instance.anchors.length > 1) {
-      runIt(instance, possibleTops, possibleTops,allAnchors )
+      runBreadth(instance, possibleTops, possibleTops,allAnchors )
     }
     else {
       val rect = GeneralGreedy.anchorIt(instance.anchors.head, new Instance(instance.topRightBox, instance.forbiddenRectangles,allAnchors), possibleTops)
@@ -30,13 +30,13 @@ object BruteForce extends Algorithm {
     * @param allAnchors
     * @return
     */
-  def runIt(instance: Instance,choices:List[Point],possibleTops:List[Point],allAnchors:List[Anchor]):Map[Anchor,Rectangle]={
+  def runBreadth(instance: Instance, choices:List[Point], possibleTops:List[Point], allAnchors:List[Anchor]):Map[Anchor,Rectangle]={
 
     if (choices.length>=1) {
       if (instance.anchors.head.dominatesStrict(choices.head) && allAnchors.forall(a=> !(a.inRectStrictUP(Rectangle(instance.anchors.head,choices.head)))) && instance.forbiddenRectangles.forall(r=> !(r.intersects(Rectangle(instance.anchors.head,choices.head)))) ) {
         val newInst=new Instance(instance.topRightBox,new Rectangle(instance.anchors.head,choices.head)+:instance.forbiddenRectangles,instance.anchors.tail)
-        val r1=runAux(newInst,possibleTops,allAnchors)
-        val r2=runIt(instance,choices.tail,possibleTops,allAnchors)
+        val r1=runDepth(newInst,possibleTops,allAnchors)
+        val r2=runBreadth(instance,choices.tail,possibleTops,allAnchors)
         if (r2.values.map(r=>r.volume).sum>(r1.values.map(r=>r.volume).sum+Rectangle(instance.anchors.head,choices.head).volume)) {
           r2
         }
@@ -46,12 +46,12 @@ object BruteForce extends Algorithm {
         }
       }
       else {
-        runIt(instance,choices.tail,possibleTops,allAnchors)
+        runBreadth(instance,choices.tail,possibleTops,allAnchors)
       }
     }
     else {
       val newInst=new Instance(instance.topRightBox,new Rectangle(instance.anchors.head,instance.anchors.head)+:instance.forbiddenRectangles,instance.anchors.tail)
-      runAux(newInst,possibleTops,allAnchors)+ (instance.anchors.head-> new Rectangle(instance.anchors.head,instance.anchors.head))
+      runDepth(newInst,possibleTops,allAnchors)+ (instance.anchors.head-> new Rectangle(instance.anchors.head,instance.anchors.head))
     }
   }
 
@@ -59,7 +59,7 @@ object BruteForce extends Algorithm {
   def run(instance:Instance):Output=  {
     require(instance.forbiddenRectangles.forall(r=>r.contained(instance.topRightBox)),"Forbidden rectangles not in the box")
     val possibleTops=instance.tops()
-    new Output(instance,runAux(instance,possibleTops,instance.anchors))
+    new Output(instance,runDepth(instance,possibleTops,instance.anchors))
   }
 
 }
